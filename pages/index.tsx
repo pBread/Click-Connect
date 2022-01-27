32e2c416333995a9368c09ab28a4a690d8a0a2da
Phil <pbrede21@gmail.com>
2022-01-27 16:43:34 -0700

    updated css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,26 @@
-import { Button, Typography, Input } from "antd";
+import { Button, Input, Divider } from "antd";
 import { useState } from "react";
 import { v4 } from "uuid";
+import styles from "../styles/Home.module.css";
 
 const PHONE = process.env.NEXT_PUBLIC_PHONE;
-const { Title } = Typography;
 
 export default function Home() {
   const [code] = useState(makeCode);
   const [id, setId] = useState(v4);
 
   return (
-    <div>
-      <h2>{code}</h2>
-      <Input value={id} />
+    <div className={styles.wrapper}>
+      <div>
+        <h5>Code</h5>
+        <h2>{code}</h2>
+      </div>
+      <div>
+        <h5>Identifier</h5>
+        <Input onChange={(ev) => setId(ev.target.value)} value={id} />
+      </div>
+
+      <Divider />
       <Button href={`tel:${PHONE},${id}#`}>Click to Connect</Button>
     </div>
   );
@@ -21,5 +29,6 @@ export default function Home() {
 function makeCode() {
   for (var i = 0, str = ""; i < 6; i++)
     str += "0123456789"[Math.floor(Math.random() * 10)];
+
   return str;
 }
