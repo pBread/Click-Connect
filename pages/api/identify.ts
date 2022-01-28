0d2a9e8c2f30dafa6b0ca7d744ac58194a653e14
Phil <pbrede21@gmail.com>
2022-01-28 11:28:34 -0700

    SMS identify working

--- pages/api/identify.ts
+++ pages/api/identify.ts
@@ -7,16 +7,15 @@ const client = Twilio(ACCOUNT_SID, AUTH_TOKEN);
 /****************************************************
  Get Handlers
 ****************************************************/
-const getIdSMS: NextApiHandler = async (req, res) => {
-  const data = getItem(getCode(req) || getId(req));
-  res.json(data);
+const getIdSMS: NextApiHandler = async ({ query }, res) => {
+  const msg = query.body as string;
+  const match = msg.match(/(\([0-9]{6}\))/);
+  if (!match) res.status(500).end();
+  else res.json(await getItem(match[0]?.replace(/\(|\)/g, "")));
 };
 
-const getIdVoice: NextApiHandler = async (req, res) => {
-  const match = req.body.body.match(/(\([0-9]{6}\))/);
-  const code = match[0]?.replace(/\(|\)/g, "");
-
-  const data = getItem(code);
+const getIdVoice: NextApiHandler = async ({ query }, res) => {
+  const data = await getItem(query.code as string);
   res.json(data);
 };
 
@@ -60,6 +59,7 @@ async function setItem(key: string, data: object) {
  Route Handler
 ****************************************************/
 const handler: NextApiHandler = async (req, res) => {
+  console.log("/api/identify==\n", req.method, req.body, req.query);
   if (req.method === "POST") return postHandler(req, res);
   else if (req.query.channel === "sms") return getIdSMS(req, res);
   else if (req.query.channel === "voice") return getIdVoice(req, res);
