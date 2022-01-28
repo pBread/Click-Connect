91b03406ffd02243eb7792246b5958d9424369cf
Phil <pbrede21@gmail.com>
2022-01-28 11:22:18 -0700

    updated identify to include both SMS & Voice

--- pages/api/identify.ts
+++ pages/api/identify.ts
@@ -5,18 +5,29 @@ const { ACCOUNT_SID, AUTH_TOKEN, SYNC_SVC_SID } = process.env;
 const client = Twilio(ACCOUNT_SID, AUTH_TOKEN);
 
 /****************************************************
- Get Handler
+ Get Handlers
 ****************************************************/
-const getHandler: NextApiHandler = async (req, res) => {
-  const data = await client.sync
-    .services(SYNC_SVC_SID)
-    .syncMaps("CodeMap")
-    .syncMapItems(getCode(req) || getId(req))
-    .fetch()
-    .then(({ data }) => data as { code: string; id: string });
-
+const getIdSMS: NextApiHandler = async (req, res) => {
+  const data = getItem(getCode(req) || getId(req));
   res.json(data);
 };
+
+const getIdVoice: NextApiHandler = async (req, res) => {
+  const match = req.body.body.match(/(\([0-9]{6}\))/);
+  const code = match[0]?.replace(/\(|\)/g, "");
+
+  const data = getItem(code);
+  res.json(data);
+};
+
+async function getItem(key: string) {
+  return await client.sync
+    .services(SYNC_SVC_SID)
+    .syncMaps("CodeMap")
+    .syncMapItems(key)
+    .fetch()
+    .then(({ data }) => data as { code: string; id: string });
+}
 
 /****************************************************
  Post Handler
@@ -49,17 +60,9 @@ async function setItem(key: string, data: object) {
  Route Handler
 ****************************************************/
 const handler: NextApiHandler = async (req, res) => {
-  switch (req.method) {
-    case "GET":
-      return getHandler(req, res);
-
-    case "POST":
-      return postHandler(req, res);
-
-    case "OPTIONS":
-      console.log("options ", req.rawHeaders);
-      res.status(200).end();
-  }
+  if (req.method === "POST") return postHandler(req, res);
+  else if (req.query.channel === "sms") return getIdSMS(req, res);
+  else if (req.query.channel === "voice") return getIdVoice(req, res);
 };
 
 export default handler;
